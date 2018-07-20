39f63b92605fc0add9f3a2b4c51cfdd6356e656b
blakeavollmer <blakeavollmer@gmail.com>
2018-07-20 11:50:40 -0700

    Update
    -Added models for resistor
    -Began working on script structure for tutorial
    -Added objects to breadboard to control holes
    -Relocated camera and models for use with hololens

---
+++ ElectricalEngineeringApp/Assets/Scripts/BreadboardHoles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadboardHoles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse");
+        transform.GetComponent<Renderer>().material.color = Color.clear;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ElectricalEngineeringApp/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour {
+
+    public List<Tutorial> Tutorials = new List<Tutorial>();
+
+    public Text expText;
+
+    private static TutorialManager instance;
+    public static TutorialManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<TutorialManager>();
+
+            if (instance == null)
+                Debug.Log("There was no Tutorial Manager Found");
+
+            return instance;
+        }
+    }
+
+    private Tutorial currentTutorial;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetNextTutorial(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentTutorial)
+            currentTutorial.CheckIfHappening();
+	}
+
+    public void CompletedTutorial()
+    {
+        SetNextTutorial(currentTutorial.Order + 1);
+    }
+
+    public void SetNextTutorial(int currentOrder)
+    {
+        currentTutorial = GetTutorialByOrder(currentOrder);
+
+        if (!currentTutorial)
+        {
+            CompletedAllTutorials();
+            return;
+        }
+
+        expText.text = currentTutorial.Explanation;
+    }
+
+    public void CompletedAllTutorials()
+    {
+        expText.text = "You have completed the tutorial!";
+
+        // Button for main menu
+    }
+
+    public Tutorial GetTutorialByOrder(int Order)
+    {
+        for (int i = 0; i< Tutorials.Count; i++)
+        {
+            if (Tutorials[i].Order == Order)
+                return Tutorials[i];
+        }
+
+        return null;
+    }
+}
